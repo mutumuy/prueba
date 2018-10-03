1c7fe9cd779387f3531a803562fac1c88e027a2a
Oscar Garcia abad <mutumuy@gmail.com>
2018-10-03 13:07:23 +0200

    Residencia

--- prueba.java
+++ prueba.java
@@ -1,6 +1,7 @@
 public class oscar{
   public static void main(String args[]){
-   System.out.print("Hola mundo");
+   System.out.print("oscar garcia abad");
    System.out.print("Bembibre");
+   System.out.print("León");
  }
 }
